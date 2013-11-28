139f1ba6b33cde6ae2eaedbbdb5629a91a1dcb9b
Jeremy Rue <jrue@calpress>
2013-11-27 21:16:03 -0800

    Lots of CSS validation fixes for less than ie7

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 <?php
